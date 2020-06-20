a6415ebe734cd53fddeb17a9a9ca666d2a31a555
Tim Leytens <tim.leytens@appreciate.be>
2020-06-20 11:34:19 +0200

    set up initials tests

--- TicTacToeTests/TicTacToeTests.swift
+++ TicTacToeTests/TicTacToeTests.swift
@@ -10,25 +10,42 @@ import XCTest
 @testable import TicTacToe
 
 class TicTacToeTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    func testGameSetup() throws {
+        let game = Game()
+        XCTAssertNotNil(game.players[0], "Validate player X not nil")
+        XCTAssertNotNil(game.players[1], "Validate player O not nil")
+        XCTAssertEqual(game.players[0].name, "X", "Validate player X name")
+        XCTAssertEqual(game.players[1].name, "O", "Validate player O name")
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testXGoesFirst() throws {
+        let game = Game()
+        XCTAssertEqual(game.currentPlayer().name, "X", "Player X goes first")
+        game.restart()
+        XCTAssertEqual(game.currentPlayer().name, "X", "Player X goes first after restart")
+        game.play(index: 0)
+        XCTAssertEqual(game.currentPlayer().name, "O", "Player O after play")
+        game.restart()
+        XCTAssertEqual(game.currentPlayer().name, "X", "Player X goes first after restart")
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testAlternatePlacing() throws {
+        let game = Game()
+        XCTAssertEqual(game.currentPlayer().name, "X", "Player X's turn")
+        game.play(index: 0)
+        XCTAssertEqual(game.currentPlayer().name, "O", "Player O's turn")
+        game.play(index: 1)
+        XCTAssertEqual(game.currentPlayer().name, "X", "Player X's turn")
+        game.play(index: 2)
+        XCTAssertEqual(game.currentPlayer().name, "O", "Player O's turn")
+        game.play(index: 3)
+        XCTAssertEqual(game.currentPlayer().name, "X", "Player X's turn")
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testWinningPlays() throws {
     }
-
+    
+    func testDraw() throws {
+    }
 }
